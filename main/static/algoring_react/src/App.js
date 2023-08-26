66c73e5da90e1f0e8f54e2f74bbe5d4b20b0530e
Justin K. Jang <jangju99@gmail.com>
2023-08-26 08:16:58 -0700

    test commit

--- main/static/algoring_react/src/App.js
+++ main/static/algoring_react/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Test</header>
+      <header className="App-header">Test2</header>
     </div>
   );
 }
