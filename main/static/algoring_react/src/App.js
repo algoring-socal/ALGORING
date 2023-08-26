02a9ece6f4a6d0039e0c0f593e2e471c4e7ecd64
Justin K. Jang <justinj@atrihub.io>
2023-08-26 08:12:24 -0700

    test commit

--- main/static/algoring_react/src/App.js
+++ main/static/algoring_react/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">HIHIHI</header>
+      <header className="App-header">Test</header>
     </div>
   );
 }
