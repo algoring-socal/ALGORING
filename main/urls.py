f45ecc790db806c4f3382afee9e9e92019f50cc7
Justin K. Jang <jangju99@gmail.com>
2023-08-26 23:27:32 -0700

    feat: url submit api complete
    - Model Update

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import re_path
 
 from main import views
 
 urlpatterns = [
-    path("", views.main_page, name="main_page"),
+    # API: url submit
+    re_path(r'^api/v1/url_submit/?$', views.UrlSubmit.as_view(), name='url_submit'),
+
+    # todo: inventory get API
+    # todo: characters data should be saved as default
+    # todo: characters display should match 3 when complete
+
+    # Render App
+    re_path('^.*$', views.main_page, name="main_page"),
 ]
