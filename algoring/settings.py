9d1b00c340d6dcc05f46d033865436914df22ffd
Justin Kyumin Jang <jangju99@gmail.com>
2023-10-17 22:48:09 -0700

    remove permission authentication

--- algoring/settings.py
+++ algoring/settings.py
@@ -156,18 +156,3 @@ TEMPLATES = [
         },
     },
 ]
-
-# REST_FRAMEWORK
-# http://www.django-rest-framework.org/api-guide/permissions/
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
-    )
-}
